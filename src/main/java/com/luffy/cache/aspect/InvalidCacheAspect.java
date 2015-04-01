617900b265a08f7d06b482b7b346120e8a968112
zhenglu <zhixuan204@sina.com>
2015-04-01 17:26:41 +0800

    去掉无用import

--- src/main/java/com/luffy/cache/aspect/InvalidCacheAspect.java
+++ src/main/java/com/luffy/cache/aspect/InvalidCacheAspect.java
@@ -1,8 +1,8 @@
 package com.luffy.cache.aspect;
 
-import com.souche.cybertron.cache.CacheService;
-import com.souche.cybertron.cache.annotation.CacheContext;
-import com.souche.cybertron.cache.annotation.InvalidCache;
+import com.luffy.cache.CacheService;
+import com.luffy.cache.annotation.CacheContext;
+import com.luffy.cache.annotation.InvalidCache;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
@@ -30,7 +30,7 @@ public class InvalidCacheAspect extends BaseAspect{
                 InvalidCache cacheData =  method.getAnnotation(InvalidCache.class);
                 cacheKey +=  extKey(cacheData.prefix(),method,pjpParam);
                 CacheService cacheService =  CacheContext.getCacheMap(cacheData.cacheType());
-                cacheService.del(cacheKey);
+                cacheService.delete(cacheKey);
 
         }catch (Exception e){
             getLogger().error("Caching on method"+pjpParam.toString() + " and key "+ cacheKey +" aborted due to an error",e);
